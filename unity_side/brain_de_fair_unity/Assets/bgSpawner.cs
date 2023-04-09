cb08ee8237a26ca9e398bc6a651852c3ceee6d83
Kgewrg <kgewrgitsaros@gmail.com>
2023-04-09 12:32:37 +0300

    added randomnes to background

--- unity_side/brain_de_fair_unity/Assets/bgSpawner.cs
+++ unity_side/brain_de_fair_unity/Assets/bgSpawner.cs
@@ -8,22 +8,32 @@ public class bgSpawner : MonoBehaviour
     public GameObject charPrefab;
     // Start is called before the first frame update
     void Start() {
+        
+        float sideDist = 1.5f;
+        float heightOffset;
+        int n_chars = 8;
 
-        for (int i = 0; i < 4; i++) {
-            Instantiate(charPrefab, new Vector3((float)(-4.5 + (i*2.5)), -2, 0), Quaternion.identity);
+        // First line (bot to top)
+        for (int i = 0; i < n_chars; i++) {
+            heightOffset = Random.Range(-1.0f, 1.0f);
+            print(heightOffset);
+            Instantiate(charPrefab, new Vector3((float)(-4.5 + (i*sideDist)), -2+heightOffset, 0), Quaternion.identity);
         }
 
-        for (int i = 0; i < 5; i++) {
-            Instantiate(charPrefab, new Vector3((float)(-5.5 + (i*2.5)), 0, 0), Quaternion.identity);
+        for (int i = 0; i < n_chars+1; i++) {
+            heightOffset = Random.Range(-1.0f, 1.0f);
+            Instantiate(charPrefab, new Vector3((float)(-5.5 + (i*sideDist)), 0+heightOffset, 0), Quaternion.identity);
         }
 
-        for (int i = 0; i < 4; i++) {
-            Instantiate(charPrefab, new Vector3((float)(-4.5 + (i*2.5)), 2, 0), Quaternion.identity);
+        for (int i = 0; i < n_chars; i++) {
+            heightOffset = Random.Range(-1.0f, 1.0f);
+            Instantiate(charPrefab, new Vector3((float)(-4.5 + (i*sideDist)), 2+heightOffset, 0), Quaternion.identity);
         }
 
 
-        for (int i = 0; i < 5; i++) {
-            Instantiate(charPrefab, new Vector3((float)(-5.5 + (i*2.5)), 4, 0), Quaternion.identity);
+        for (int i = 0; i < n_chars+1; i++) {
+            heightOffset = Random.Range(-1.0f, 1.0f);
+            Instantiate(charPrefab, new Vector3((float)(-5.5 + (i*sideDist)), 4+heightOffset, 0), Quaternion.identity);
         }
         
     }
