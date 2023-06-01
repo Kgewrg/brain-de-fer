28aa350514e5e2a53b4f1362092d16a0f59a5e83
Dounas P <ntinospalegas@gmail.com>
2023-06-01 14:44:08 +0300

    updated file

--- python_side/SimpleNeuralTesting.py
+++ python_side/SimpleNeuralTesting.py
@@ -4,6 +4,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
 import pandas as pd
 import numpy as np
 import scipy
@@ -14,17 +16,60 @@ def main():
     datasets_names="C:/Users/Dounas P/Desktop/brain-de-fair/mindwavealldata.csv"
     dataset = pd.read_csv (datasets_names)
     dataset=dataset.iloc[:,:].values
-    X=dataset[:,0:4]
+    N=9
+    counter=0
+    X=dataset[:,0:N]
+    Y=dataset[:,-1]
+    X_dataset=[]
+    Y_dataset=[]
+    X_dataset.append(dataset[0][0:N])
+    Y_dataset.append(dataset[0][-1])
+    for i in range(len(dataset[:])):
+        counter+=1
+        if( counter == 60 ):#mia timh kathe 1 deutero ara 60 times to deutero(peripou)
+            counter=0
+            X_dataset.append(dataset[i][0:N])
+            Y_dataset.append(dataset[i][-1])
+    
+    #splitMethod(X_dataset,Y_dataset)
+    crossVal(X_dataset,Y_dataset)
+
     
 
-    y=dataset[:,-1]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=None,shuffle=True)  
+def crossVal(X_dataset,Y_dataset):
+    cross_validator=KFold(n_splits=10,shuffle=True)
+    #models-----------------------------------
+    KNN =  KNeighborsClassifier(n_neighbors=11)
+    naiveBayes =  GaussianNB()
+    rForest = RandomForestClassifier(n_estimators=150, max_depth=10)
+    #SVM =svm.SVC(cache_size=1000)
+    #-----------------------------------------
+    scoresKNN = cross_val_score(KNN, X_dataset, Y_dataset, cv=cross_validator)
+    scoresNaive = cross_val_score(naiveBayes, X_dataset, Y_dataset, cv=cross_validator)
+    scoresForest = cross_val_score(rForest, X_dataset, Y_dataset, cv=cross_validator)
+    #scoresSVM = cross_val_score(SVM, X_dataset, Y_dataset, cv=cross_validator)
+    # Calculate and print the mean accuracy across all folds
+    
+    mean_accuracy = scoresKNN.mean()
+    print(f"KNN: {mean_accuracy*100}")
+    mean_accuracy = scoresNaive.mean()
+    print(f"Naive: {mean_accuracy*100}")
+    mean_accuracy = scoresForest.mean()
+    print(f"Forest: {mean_accuracy*100}")
+    # mean_accuracy = scoresSVM.mean()
+    # print(f"SVM: {mean_accuracy*100}")
+
+
+
+
+def splitMethod(X_dataset,Y_dataset):
+    X_train, X_test, y_train, y_test = train_test_split(X_dataset, Y_dataset, test_size=0.2, random_state=None,shuffle=True)  
     array=[] 
 
     #initialize the KNN classifier
-    naiveBayes =  KNeighborsClassifier(n_neighbors=11)
-    naiveBayes.fit(X_train,y_train)
-    y_pred=naiveBayes.predict(X_test)
+    KNN =  KNeighborsClassifier(n_neighbors=11)
+    KNN.fit(X_train,y_train)
+    y_pred=KNN.predict(X_test)
     array.append(accuracy_score(y_test,y_pred)*100)
     print("----KNN Complete----")
     #initialize the naiveBayes classifier
@@ -35,18 +80,18 @@ def main():
     print("----Gaussian Bayes Complete----")
     
     #initialize the RandomForest classifier
-    naiveBayes = RandomForestClassifier(n_estimators=100, random_state=None)
-    naiveBayes.fit(X_train,y_train)
-    y_pred=naiveBayes.predict(X_test)
+    rForest = RandomForestClassifier(n_estimators=100, random_state=None)
+    rForest.fit(X_train,y_train)
+    y_pred=rForest.predict(X_test)
     array.append(accuracy_score(y_test,y_pred)*100)
     print("----Randomforest Complete----")
     
-    # #initialize the SVM classifier
-    # naiveBayes =svm.SVC(kernel='linear')#SVC->classification SVR->Regression
-    # naiveBayes.fit(X_train,y_train)
-    # y_pred=naiveBayes.predict(X_test)
-    # array.append(accuracy_score(y_test,y_pred)*100)
-    # print("----SVM Complete----")
+    #initialize the SVM classifier
+    SVM =svm.SVC(cache_size=1000)#SVC->classification SVR->Regression
+    SVM.fit(X_train,y_train)
+    y_pred=SVM.predict(X_test)
+    array.append(accuracy_score(y_test,y_pred)*100)
+    print("----SVM Complete----")
 
     print(array)
 
