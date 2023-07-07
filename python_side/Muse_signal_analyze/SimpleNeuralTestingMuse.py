dcf324acd0b14c03d85bfd71be3adf832adc7e87
Dounas P <ntinospalegas@gmail.com>
2023-07-07 16:59:54 +0300

    small changes

--- python_side/Muse_signal_analyze/SimpleNeuralTestingMuse.py
+++ python_side/Muse_signal_analyze/SimpleNeuralTestingMuse.py
@@ -14,19 +14,16 @@ import scipy
 
 
 def main():
-    N=4 #size of dataset excluding category column
     # dataset
     datasets_names="C:/Users/Dounas P/Desktop/brain-de-fair/MuseALL.csv"
     dataset = pd.read_csv(datasets_names)
     dataset=dataset.iloc[:,:].values
-    DatasetX=dataset[:,0:N]
+    DatasetX=dataset[:,:-1]
     DatasetY=dataset[:,-1]
-    splitMethod(DatasetX,DatasetY)
+    #splitMethod(DatasetX,DatasetY)
     
     #splitMethod(DatasetX,DatasetY)
-    #crossVal(DatasetX,DatasetY)
-    
-
+    crossVal(DatasetX,DatasetY)
     
 
 def crossVal(X_dataset,Y_dataset):
@@ -35,12 +32,14 @@ def crossVal(X_dataset,Y_dataset):
     KNN =  KNeighborsClassifier(n_neighbors=11)
     naiveBayes =  GaussianNB()
     rForest = RandomForestClassifier(n_estimators=150, max_depth=10)
-    #SVM =svm.SVC(cache_size=1000)
+    SVM =svm.SVC(cache_size=1000)
+    MLP = MLPClassifier(hidden_layer_sizes=(64), activation='relu', solver='adam',max_iter=600)
     #-----------------------------------------
     scoresKNN = cross_val_score(KNN, X_dataset, Y_dataset, cv=cross_validator)
     scoresNaive = cross_val_score(naiveBayes, X_dataset, Y_dataset, cv=cross_validator)
     scoresForest = cross_val_score(rForest, X_dataset, Y_dataset, cv=cross_validator)
-    #scoresSVM = cross_val_score(SVM, X_dataset, Y_dataset, cv=cross_validator)
+    scoresSVM = cross_val_score(SVM, X_dataset, Y_dataset, cv=cross_validator)
+    scoresMLP = cross_val_score(MLP, X_dataset, Y_dataset, cv=cross_validator)
     # Calculate and print the mean accuracy across all folds
     
     mean_accuracy = scoresKNN.mean()
@@ -49,49 +48,48 @@ def crossVal(X_dataset,Y_dataset):
     print(f"Naive: {mean_accuracy*100}")
     mean_accuracy = scoresForest.mean()
     print(f"Forest: {mean_accuracy*100}")
-    # mean_accuracy = scoresSVM.mean()
-    # print(f"SVM: {mean_accuracy*100}")
+    mean_accuracy = scoresSVM.mean()
+    print(f"SVM: {mean_accuracy*100}")
+    mean_accuracy = scoresMLP.mean()
+    print(f"MLP: {mean_accuracy*100}")
     
-
-
-
 def splitMethod(X_dataset,Y_dataset):
     
     x_train, x_test, y_train, y_test = train_test_split(X_dataset, Y_dataset, test_size=20, random_state=None,shuffle=True)  
     array=[] 
 
-    #initialize the KNN classifier
-    KNN =  KNeighborsClassifier(n_neighbors=11)
-    KNN.fit(x_train,y_train)
-    y_pred=KNN.predict(x_test)
-    array.append(accuracy_score(y_test,y_pred)*100)
-    print("----KNN Complete----")
+    # #initialize the KNN classifier
+    # KNN =  KNeighborsClassifier(n_neighbors=11)
+    # KNN.fit(x_train,y_train)
+    # y_pred=KNN.predict(x_test)
+    # array.append(accuracy_score(y_test,y_pred)*100)
+    # print("----KNN Complete----")
 
-    #initialize the naiveBayes classifier
-    naiveBayes =  GaussianNB()
-    naiveBayes.fit(x_train,y_train)
-    # joblib.dump(naiveBayes,"naiveBayes.pkl")
-    y_pred=naiveBayes.predict(x_test)
-    array.append(accuracy_score(y_test,y_pred)*100)
-    print("----Gaussian Bayes Complete----")
+    # #initialize the ΝaiveBayes classifier
+    # naiveBayes =  GaussianNB()
+    # naiveBayes.fit(x_train,y_train)
+    # # joblib.dump(naiveBayes,"naiveBayes.pkl")
+    # y_pred=naiveBayes.predict(x_test)
+    # array.append(accuracy_score(y_test,y_pred)*100)
+    # print("----Gaussian Bayes Complete----")
     
-    #initialize the RandomForest classifier
-    rForest = RandomForestClassifier(n_estimators=100, max_depth=12)
-    rForest.fit(x_train,y_train)
-    # joblib.dump(rForest,"RandomForestModel.pkl")
-    y_pred=rForest.predict(x_test)
-    array.append(accuracy_score(y_test,y_pred)*100)
-    print("----Randomforest Complete----")
-    print(array)
-    #initialize the SVM classifier
-    # SVM =svm.SVC(cache_size=1000)#SVC->classification SVR->Regression
-    # SVM.fit(X_train,y_train)
-    # # joblib.dump(SVM,"SVMModel.pkl")
-    # y_pred=SVM.predict(X_test)
-    # array.append(accuracy_score(Y_test,y_pred)*100)
-    # print("----SVM Complete----")
+    # #initialize the RandomForest classifier
+    # rForest = RandomForestClassifier(n_estimators=100, max_depth=12)
+    # rForest.fit(x_train,y_train)
+    # # joblib.dump(rForest,"RandomForestModel.pkl")
+    # y_pred=rForest.predict(x_test)
+    # array.append(accuracy_score(y_test,y_pred)*100)
+    # print("----Randomforest Complete----")
+    # print(array)
+    # #initialize the SVM classifier
+    # # SVM =svm.SVC(cache_size=1000)#SVC->classification SVR->Regression
+    # # SVM.fit(X_train,y_train)
+    # # # joblib.dump(SVM,"SVMModel.pkl")
+    # # y_pred=SVM.predict(X_test)
+    # # array.append(accuracy_score(Y_test,y_pred)*100)
+    # # print("----SVM Complete----")
 
-    mlp = MLPClassifier(hidden_layer_sizes=(64, 64), activation='relu', solver='adam', random_state=42)
+    mlp = MLPClassifier(hidden_layer_sizes=(64), activation='relu', solver='adam',max_iter=600)
     mlp.fit(x_train, y_train)
     y_pred = mlp.predict(x_test)
     array.append(accuracy_score(y_test, y_pred)*100)
