f3c4ef834b7081d24bfe040c930bf76562e88398
Dounas P <ntinospalegas@gmail.com>
2023-07-12 19:50:47 +0300

    den teleiwse h klhsh tou swmatos akoma

---
+++ unity_side/brain_de_fair_unity/Assets/Position.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Position : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Slider slider;
+    public Transform StartAxis;
+    public Transform Lose;
+    public Transform Win;
+
+    private void Start()
+    {
+        transform.localPosition = StartAxis.localPosition;
+    }
+
+    private void Update()
+    {
+        // float t = slider.value / slider.maxValue;
+        // Vector3 targetPosition = Vector3.Lerp(Lose.localPosition, Win.localPosition, t);
+        // if(slider.value  >= slider.maxValue / 2)
+        //     transform.localPosition = Vector3.Lerp(StartAxis.localPosition, targetPosition, t);
+        // else
+        //     transform.localPosition = Vector3.Lerp(targetPosition , StartAxis.localPosition, t);
+    }
+}
