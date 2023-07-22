d50141313d41e79e045aea630a4a0971453eb50c
Kgewrg <kgewrgitsaros@gmail.com>
2023-07-22 14:18:51 +0300

    Εγινε σχεδον το wrestle κομμάτι

--- unity_side/brain_de_fair_unity/Assets/WrestleTargetScript.cs
+++ unity_side/brain_de_fair_unity/Assets/WrestleTargetScript.cs
@@ -6,14 +6,15 @@ public class WrestleTargetScript : MonoBehaviour
 {
     public GameObject pivotPoint;
     // public GameObject target; 
-    [Range(-80, 80)]
-    public float angle; // [-90, 90]
+    [Range(-60, 60)]
+    public float angle; 
 
     private float radius;
-    private float pY, pZ;
+    private float pX, pY, pZ;
     private float x, y, z;
 
     void Start() {
+        pX = pivotPoint.transform.position.x;
         pY = pivotPoint.transform.position.y;
         pZ = pivotPoint.transform.position.z;
         
@@ -24,12 +25,16 @@ public class WrestleTargetScript : MonoBehaviour
     }
 
     void Update() {
-        x = transform.position.x;
+
+        x = pX + Remap(angle, -60.0f, 60.0f, -0.05f, 0.05f);
         y = pY + (radius * Mathf.Cos(Mathf.Deg2Rad * angle));
         z = pZ + (radius * Mathf.Sin(Mathf.Deg2Rad * angle));
 
         transform.position = new Vector3(x, y, z);
 
 
+    }
+    float Remap (float value, float from1, float to1, float from2, float to2) {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
     }
 }
