d9765d4a89dac4db9cd2af57987a363b95d205d1
Kgewrg <kgewrgitsaros@gmail.com>
2023-09-07 15:31:56 +0300

    άλλαξα τα χρώματα, startmenu και έβαλα health bars

--- unity_side/brain_de_fair_unity/Assets/UserInterfaceSricpt.cs
+++ unity_side/brain_de_fair_unity/Assets/UserInterfaceSricpt.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UserInterfaceSricpt : MonoBehaviour
 {
@@ -11,8 +12,18 @@ public class UserInterfaceSricpt : MonoBehaviour
     public GameObject conStatusMessage_container;
     // public TMP_Text P2_EGG_text;
 
+    public TMP_Text rightP_TextBox;
+
+    public Slider LeftPlayerHealth;
+    public Slider RightPlayerHealth;
+
+    private float sliderValue;
+
     void Start() {
         conStatusMessage_container.SetActive(false);
+
+        
+
     }
 
     void Update() {
@@ -32,6 +43,26 @@ public class UserInterfaceSricpt : MonoBehaviour
         //     P1_EGG_obj.SetActive(false);
 
         // }
+
+
+        if (PlayerPrefs.GetInt("gameMode", -1) == 1){
+            rightP_TextBox.SetText("Bot's health");
+        }
+        else {
+            rightP_TextBox.SetText("right player's health");
+        }
+
+        sliderValue = Sliderscript.publicSliderValue - 5;
+        LeftPlayerHealth.value = Remap(sliderValue, 0, -5, 0, 1);
+        RightPlayerHealth.value = Remap(sliderValue, 0, 5, 0, 1); 
+
+
+
+
                       
+    }
+
+    float Remap (float value, float from1, float to1, float from2, float to2) {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
     }
 }
